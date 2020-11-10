3ca725c13b99a8c9c012daf7f22a9e51e4d82fa8
root <nishaajithkj@gmail.com>
2020-11-10 05:45:35 -0600

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+lear
+cat banner.txt
+echo "Starting..."
+sleep 2
+nano spam_text
+echo "How many times would you like to loop:"
+read f
+sleep 5
+for ((i=0; i<$f; i++))
+do
+python3 SpamBomb.py
+done
+echo "--------------Sending complete----------------"
