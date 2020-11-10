a7fdfd5aba2792a1fc32978e62e5eb31c93bd7a8
root <nishaajithkj@gmail.com>
2020-11-10 05:49:37 -0600

    Updated SpamBomb.py

--- SpamBomb.py
+++ SpamBomb.py
@@ -1,14 +1,5 @@
 import pyautogui, time
-print('starting spamming, please wait 5 second')
-time.sleep(5)
 f = open("spam_text" , 'r' , encoding='utf-8')
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
-    print('sending...')
-print('=========================')
-time.sleep(1)
-print('Spamming completed')
-time.sleep(2)
-print('Exiting...')
-time.sleep(3)
